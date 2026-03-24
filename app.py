61513e696cbe6f77765b611efd5ad520bd7deb58
FRI Developer <fri@fisheries.gov.tw>
2026-03-24 12:38:09 +0800

    fix: allow werkzeug dev server + add launch config

--- app.py
+++ app.py
@@ -500,4 +500,4 @@ def ws_summarize(data):
 if __name__ == '__main__':
     init_db()
     port = int(os.environ.get('PORT', 5000))
-    socketio.run(app, debug=False, host='0.0.0.0', port=port)
+    socketio.run(app, debug=False, host='0.0.0.0', port=port, allow_unsafe_werkzeug=True)
